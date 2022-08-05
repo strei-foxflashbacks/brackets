65241d06bc0401c75013f3b1b8f67b797402046f
ssekridov@gmail.com <ssekridov@gmail.com>
2022-08-06 02:47:21 +0300

    feat task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OPEN_BRS = []
+  const PAIR_BRS = {}
+  
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let key = bracketsConfig[i][1]
+    let value = bracketsConfig[i][0]
+    OPEN_BRS.push(value)
+    PAIR_BRS[key] = value
+  }
+
+  let stack = []
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i]
+    let topEl = stack[stack.length - 1]
+
+    if (OPEN_BRS.includes(currentSymbol)) {
+      (topEl === currentSymbol) && (PAIR_BRS[currentSymbol] === currentSymbol) ? stack.pop() : stack.push(currentSymbol)
+    }
+    else {
+      if (stack.length === 0) {
+        return false
+      }
+      if (PAIR_BRS[currentSymbol] === topEl) {
+        stack.pop()
+      }
+      else {
+        return false
+      }
+    }
+  }
+  return stack.length == 0
 }
